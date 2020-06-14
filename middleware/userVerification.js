46e6a5647d21c0395697311f09837d78e189ba4c
thiagohilpert <thiago@agenciath2.com.br>
2020-06-14 17:14:18 -0300

    COnflict Solve

--- middleware/userVerification.js
+++ middleware/userVerification.js
@@ -5,13 +5,6 @@ const userVerification = async (req, res, next) => {
     if( await userModel.findOne( { email } ) ){
         return res.status(400).send( { error: 'User already exists, try another email' } );
     }else{
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        userModel.password = undefined;
-=======
->>>>>>> 1c7bcf3d6cd7bf38b39177a42be73714233d85ae
->>>>>>> 51f25b3157b334e0dc44f91f7ab0835893567f73
         next();
     }
 }
