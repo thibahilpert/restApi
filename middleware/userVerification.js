1c7bcf3d6cd7bf38b39177a42be73714233d85ae
thiagohilpert <thiago@agenciath2.com.br>
2020-06-14 16:56:48 -0300

    Feat: User Verification middleware

--- middleware/userVerification.js
+++ middleware/userVerification.js
@@ -1,10 +1,12 @@
-const userModel = require('./userModel');
+const userModel = require('../models/user/userModel');
 
-const userVerification = (req, res, next) => {
+const userVerification = async (req, res, next) => {
     const { email } = req.body;
     if( await userModel.findOne( { email } ) ){
-        
+        return res.status(400).send( { error: 'User already exists, try another email' } );
+    }else{
+        next();
     }
 }
 
-module.exports = userVerification;
+module.exports.userVerification = userVerification;
